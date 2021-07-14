f2412e0e5b6f428dca2fe62fd88de47d604b94f8
Erica Slotkin <77819164+ferris4936@users.noreply.github.com>
2021-07-13 19:37:06 -0600

    Add project files.

---
+++ HourlyEmployee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slotkin_InheritanceDemo
+{
+    public class HourlyEmployee : Employee
+    {
+        public decimal HourlyRate { get; set; }
+
+        public HourlyEmployee() : base()
+        {
+            HourlyRate = 15.0M;
+        }
+
+        //public override string GetPaySummary()
+        //{
+        //    return "This employee is payed " + HourlyRate + " per hour";
+        //}
+
+        //new public string GetPaySummary()
+        //{
+        //    return "This employee is payed " + HourlyRate + " per hour";
+        //}
+
+        public override string PaySummary
+        {
+            get { return "This employee is payed " + HourlyRate + " per hour"; }
+        }
+
+
+        new public string ToString()
+        {
+            return base.ToString() + " Hourly Employee";
+        }
+    }
+}
